d0a9c2ff807d850afe25b9a3642b610f41e510fc
GSerdg <grebelsergey@mail.ru>
2023-04-13 16:32:48 +0300

    part 3

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -15,11 +15,39 @@ const { NotImplementedError } = require('../extensions/index.js');
  * => 'STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS'
  *
  */
-function repeater(/* str, options */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
-}
+ function repeater(str, options) {
+  let tmp = [];
+  let strAddition = '';
+  let addition = options.addition;
+  let additionSeparator = options.additionSeparator || '|';
+  let separator = options.separator || '+';
+  let additionRepeatTimes = options.additionRepeatTimes || 1;
+  let repeatTimes = options.repeatTimes || 1;
 
+  if (str === null) {
+    str = 'null';
+  } else if (typeof(str) !== 'string' && typeof(str) !== 'object') {
+    str = str.toString();
+  }
+  if (addition === null) {
+    addition = 'null';
+  } else if (typeof(addition) !== 'string' && typeof(addition) !== 'undefined' && typeof(addition) !== 'object') {
+    addition = addition.toString();
+  }
+
+  if (addition !== "") {
+    for (let i = 0; i < additionRepeatTimes; i++) {
+      tmp.push(addition);
+    }
+    strAddition = tmp.join(additionSeparator);
+    tmp = [];
+  }
+  str = str + strAddition;
+  for (let i = 0; i < repeatTimes; i++) {
+    tmp.push(str);
+  }
+  return tmp.join(separator);
+}
 module.exports = {
   repeater
 };
--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -20,15 +20,107 @@ const { NotImplementedError } = require('../extensions/index.js');
  * 
  */
 class VigenereCipheringMachine {
-  encrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  constructor(type) {
+    this.type = type;
+    this.abc = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
   }
-  decrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  encrypt(str, key) {
+    try {
+      if (str === undefined || key === undefined) {
+        throw new Error('Incorrect arguments!');
+      }
+    } catch (error) {
+      throw error;
+    }
+    str = str.toUpperCase();
+    key = key.toUpperCase();
+    let tabula = this.tabula();
+    let tmpCriptKey = "";
+    let criptKey = '';
+    let encode = '';
+    let j = 0;
+    while (str.length > tmpCriptKey.length) {
+      tmpCriptKey += key;
+    }
+    for (const item of str) {
+      if (item === " ") {
+        criptKey += " ";
+        continue;
+      }
+      criptKey += tmpCriptKey[j]
+      j++;
+    }
+    let number;
+    for (let i = 0; i < str.length; i++) {
+      if (tabula[str[i]] === undefined) {
+        encode += str[i];
+      } else {
+        number = tabula[str[i]] + tabula[criptKey[i]];
+        if (number >= this.abc.length) {
+          number = number - this.abc.length;
+        }
+        encode += this.abc[number];
+      }
+    }
+    if (this.type === true || this.type === undefined) {
+      return encode;
+    }
+    return encode.split("").reverse().join("");
+  }
+  decrypt(str, key) {
+    try {
+      if (str === undefined || key === undefined) {
+        throw new Error('Incorrect arguments!');
+      }
+    } catch (error) {
+      throw error;
+    }
+    str = str.toUpperCase();
+    key = key.toUpperCase();
+    let tabula = this.tabula();
+    let tmpCriptKey = "";
+    let criptKey = "";
+    let decode = '';
+    let j = 0;
+    while (str.length > tmpCriptKey.length) {
+      tmpCriptKey += key;
+    }
+    for (const item of str) {
+      if (item === " ") {
+        criptKey += " ";
+        continue;
+      }
+      criptKey += tmpCriptKey[j]
+      j++;
+    }
+    let number;
+    for (let i = 0; i < str.length; i++) {
+      if (tabula[str[i]] === undefined) {
+        decode += str[i];
+      } else {
+        number = tabula[str[i]] - tabula[criptKey[i]];
+        if (number < 0) {
+          number = number + this.abc.length;
+        }
+        decode += this.abc[number];
+      }
+    }
+    if (this.type === true || this.type === undefined) {
+      return decode;
+    }
+    return decode.split("").reverse().join("");
+  }
+
+  tabula() {
+    let tabula = this.abc.split("");
+    tabula = tabula.reduce(function (object, value, index) {
+      object[value] = index;
+      return object;
+    }, {});
+    return tabula;
   }
 }
+
 
 module.exports = {
   VigenereCipheringMachine
